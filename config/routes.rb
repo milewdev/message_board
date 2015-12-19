700bb4c16d57663ddc42acd9f56510d6f2c53ce3
milewdev <milewdev@gmail.com>
2015-12-19 16:57:58 -0500

    Point root at readonly page.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :boards
   get 'boards/:id/readonly' => 'boards#readonly'
 
-  root to: 'boards#show', id: 1, as: :home
+  root to: 'boards#readonly', id: 1, as: :home
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
